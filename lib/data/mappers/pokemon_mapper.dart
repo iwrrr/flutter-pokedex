2d286583cd3e10b77d23240d332eeadf7f5b811a
Irya <iryadev15@gmail.com>
2025-09-09 01:07:50 +0700

    feat: add pokemon mapper and repository

---
+++ lib/data/mappers/pokemon_mapper.dart
@@ -0,0 +1,9 @@
+import 'package:pokedex/domain/models/pokemon.dart';
+import 'package:pokedex/infrastructure/network/responses/pokemon_dto.dart';
+
+class PokemonMapper {
+  const PokemonMapper._();
+
+  static Pokemon fromRemote(PokemonDto dto) =>
+      Pokemon(name: dto.name ?? '', url: dto.url ?? '');
+}
---
+++ lib/data/repositories/pokemon_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:pokedex/core/exception/error_mapper.dart';
+import 'package:pokedex/core/exception/failure.dart';
+import 'package:pokedex/data/mappers/pokemon_mapper.dart';
+import 'package:pokedex/domain/models/pokemon.dart';
+import 'package:pokedex/domain/repositories/pokemon_repository.dart';
+import 'package:pokedex/infrastructure/network/services/pokemon_service.dart';
+
+class PokemonRepositoryImpl extends PokemonRepository {
+  final PokemonService _pokemonService;
+
+  PokemonRepositoryImpl({required PokemonService pokemonService})
+    : _pokemonService = pokemonService;
+
+  @override
+  Future<Either<Failure, List<Pokemon>>> getPokemons({
+    required int limit,
+    required int offset,
+  }) async {
+    try {
+      final response = await _pokemonService.getPokemons(limit, offset);
+      return Right(
+        response.results?.map(PokemonMapper.fromRemote).toList() ?? [],
+      );
+    } catch (e, stack) {
+      return Left(ErrorMapper.map(e, stackTrace: stack));
+    }
+  }
+}
